7e45d9a04ac42c7ad23eeaa831ba9fe673d2c77f
Ran Chen <FredChen@Rans-MacBook-Pro.local>
2017-04-26 15:23:12 -0400

    mlab url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/gallery',
+    'mongoUrl' : 'mongodb://<dbuser>:<dbpassword>@ds121091.mlab.com:21091/heroku_slcksm5f',
     'facebook': {
         clientID: '402464210109451',
         clientSecret: '17de3f6fbfcb1c01b6fa325f19984cab',
